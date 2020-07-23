9f6f0d827d0f38bbadaf363c6e11ffe0ddcc7949
Matheus Navarro <matheusnavarro@alu.ufc.br>
2020-07-23 19:40:00 -0300

    #12 att tempo de expiracao do token -> 4 horas

--- app/Http/Controllers/Api/JWTAuthController.php
+++ app/Http/Controllers/Api/JWTAuthController.php
@@ -108,7 +108,7 @@ class JWTAuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 60
+            'expires_in' => auth('api')->factory()->getTTL() * 60 * 4
         ]);
     }
 }
