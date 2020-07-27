8f29ddcc2af3bdf88341d7a4e18c6674cc0c54c3
Moises Rodrigues <moises.rodrigues@unifametro.edu.br>
2020-07-27 10:23:43 -0300

    adiciona migration tipos_suporte_respitario, tabela pivot suporte_respiratorio_paciente e seed tipos_suporterespitario #18

--- database/migrations/2020_07_27_122149_create_tipos_suporterespiratorio_table.php
+++ database/migrations/2020_07_27_122149_create_tipos_suporterespiratorio_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColunasUsersTable extends Migration
+class CreateTiposSuporterespiratorioTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class AddColunasUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('pacientes', function (Blueprint $table) {
-            $table->boolean('paciente_suporte_respiratorio')->nullable();
-            $table->string('suporte_respiratorio')->nullable();
-            $table->boolean('reinternacao')->nullable();
+        Schema::create('tipos_suporterespiratorio', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +27,6 @@ class AddColunasUsersTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('tipos_suporterespiratorio');
     }
 }
