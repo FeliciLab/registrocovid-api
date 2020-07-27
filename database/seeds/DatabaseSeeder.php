5e007eff5b5c32acdbd8de67461410bef823e0e1
Matheus Navarro <matheusnavarro@alu.ufc.br>
2020-07-27 13:50:25 -0300

    #23 add seeder drogas

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             BairrosSeeder::class,
             InstituicoesSeeder::class,
             SituacaoUsoDrogasSeeder::class,
+            DrogasSeeder::class,
         ]);
 
     }
