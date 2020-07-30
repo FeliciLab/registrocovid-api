a89416723c5786da7173c7b9a2c9ba480826d945
Fernando Fabricio dos Santos <fernandofabricio.d@thoughtworks.com>
2020-07-30 14:46:15 -0300

    setup dos testes e limpeza de arquivos

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,34 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use DatabaseTransactions;
+
+    /**
+     * @var User
+     */
+    protected $currentUser;
+
+    public function authenticated(array $userAttributes = []): void
+    {
+        $user = factory(User::class)->create($userAttributes);
+        $this->currentUser = $user;
+
+        $this->be($user);
+
+        $token = JWTAuth::fromUser($user);
+        JWTAuth::setToken($token);
+        $this->defaultHeaders = [
+            'Authorization' => 'Bearer ' . $token
+        ];
+    }
 }
