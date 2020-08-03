6eaf39633ba89eb4e9ed0e3e61324a69d6b3d05e
Matheus Navarro <matheusnavarro@alu.ufc.br>
2020-08-03 12:48:05 -0300

    #24 add seeds comorbidade_doenca

--- database/seeds/DoencasSeeder.php
+++ database/seeds/DoencasSeeder.php
@@ -12,11 +12,11 @@ class DoencasSeeder extends Seeder
     public function run()
     {
         DB::table('doencas')->insert(['id' => 1,'tipo_doenca_id' => 1,'descricao' => 'Doença arterial coronariana']);
-        DB::table('doencas')->insert(['id' => 1,'tipo_doenca_id' => 1,'descricao' => 'Insuficiência cardíaca congestiva']);
-        DB::table('doencas')->insert(['id' => 1,'tipo_doenca_id' => 1,'descricao' => 'Arritmia cardíaca']);
-        DB::table('doencas')->insert(['id' => 1,'tipo_doenca_id' => 1,'descricao' => 'Cardiopatia não-especificada']);
-        DB::table('doencas')->insert(['id' => 1,'tipo_doenca_id' => 2,'descricao' => 'Insuficiencia venosa']);
-        DB::table('doencas')->insert(['id' => 1,'tipo_doenca_id' => 3,'descricao' => 'Doença pulmonar obstrutiva crônica']);
-        DB::table('doencas')->insert(['id' => 1,'tipo_doenca_id' => 3,'descricao' => 'Asma']);
+        DB::table('doencas')->insert(['id' => 2,'tipo_doenca_id' => 1,'descricao' => 'Insuficiência cardíaca congestiva']);
+        DB::table('doencas')->insert(['id' => 3,'tipo_doenca_id' => 1,'descricao' => 'Arritmia cardíaca']);
+        DB::table('doencas')->insert(['id' => 4,'tipo_doenca_id' => 1,'descricao' => 'Cardiopatia não-especificada']);
+        DB::table('doencas')->insert(['id' => 5,'tipo_doenca_id' => 2,'descricao' => 'Insuficiencia venosa']);
+        DB::table('doencas')->insert(['id' => 7,'tipo_doenca_id' => 3,'descricao' => 'Doença pulmonar obstrutiva crônica']);
+        DB::table('doencas')->insert(['id' => 8,'tipo_doenca_id' => 3,'descricao' => 'Asma']);
     }
 }
