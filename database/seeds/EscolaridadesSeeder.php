00247b88a998b4ca049dcdc5955170d83648c7cd
Fernanda Dias <fernandasd26@gmail.com>
2020-08-05 14:58:03 -0300

    #42 Criação de seeds para as tabelas Escolaridade e Atividades Profissionais

--- database/seeds/EscolaridadesSeeder.php
+++ database/seeds/EscolaridadesSeeder.php
@@ -11,6 +11,13 @@ class EscolaridadesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('escolaridades')->insert(['id' => 1,'nome' => 'Analfabeto']);
+        DB::table('escolaridades')->insert(['id' => 2,'nome' => 'Ensino Fundamental Incompleto (1ºG)']);
+        DB::table('escolaridades')->insert(['id' => 3,'nome' => 'Ensino Fundamental Completo']);
+        DB::table('escolaridades')->insert(['id' => 4,'nome' => 'Ensino Médio Incompleto (2ºG)']);
+        DB::table('escolaridades')->insert(['id' => 6,'nome' => 'Ensino Médio Completo']);
+        DB::table('escolaridades')->insert(['id' => 7,'nome' => 'Ensino Superior Incompleto']);
+        DB::table('escolaridades')->insert(['id' => 8,'nome' => 'Ensino Superior Completo']);
+        DB::table('escolaridades')->insert(['id' => 9,'nome' => 'Pós-Graduação']);
     }
 }
