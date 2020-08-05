8fb321050f30ff08bd1343ecd0ed23ac8625698c
Moises Rodrigues <moisesabreurodrigues@gmail.com>
2020-08-05 20:55:43 -0300

    cria migration rt_pcr_resultado e seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            RtPcrResultadoSeeder::class,
             SitioTipoSeeder::class,
             CoresSeeder::class,
             EstadoCivisSeeder::class,
