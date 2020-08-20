a0e4125119842af9a62ff6ee1fb7dedc606fa85c
moisesrodriguesdev <moisesabreurodrigues@gmail.com>
2020-08-19 22:36:22 -0300

    adiciona metodo para retornar complicacoes e refatora metodo store #56

--- app/Http/Controllers/Api/Paciente/ComplicacoesVentilacaoMec/ComplicacoesVentilacaoMecController.php
+++ app/Http/Controllers/Api/Paciente/ComplicacoesVentilacaoMec/ComplicacoesVentilacaoMecController.php
@@ -10,15 +10,37 @@ use Illuminate\Http\Request;
 
 class ComplicacoesVentilacaoMecController extends Controller
 {
+    public function index($pacienteId)
+    {
+        $complicacaoVentilacaoMec = ComplicacaoVentilacaoMec::where('paciente_id', $pacienteId)->get()->toArray();
+        $transfusaoOcorrencia = TransfusaoOcorrencia::where('paciente_id', $pacienteId)->get()->toArray();
+
+        if(!count($complicacaoVentilacaoMec) && !count($transfusaoOcorrencia)){
+            return response()->json(['message' => 'Paciente não possui complicações cadastradas'], 404);
+        }
+
+        return response()->json([
+            'complicacoes_ventilacao_mecanica' => $complicacaoVentilacaoMec,
+            'transfussoes_ocorrencia' => $transfusaoOcorrencia
+        ]);
+    }
     public function store(Request $request, $pacienteId)
     {
-        if($request->has(['tipo_complicacao_id', 'data_ventilacao_mec'])) {
+        if($request->has(['tipo_complicacao_id', 'data_complicacao'])) {
             $complicacaoVentilacaoMec = ComplicacaoVentilacaoMec::create(array_merge(
-                $request->only(['tipo_complicacao_id', 'data_ventilacao_mec', 'descricao']),
+                $request->only(['tipo_complicacao_id', 'data_complicacao', 'descricao']),
                 [
                     'paciente_id' => $pacienteId
                 ]
             ));
+
+            if($request->input('tipo_complicacao_id') == 4) {
+                $transfusaoOcorrencia = new TransfusaoOcorrencia();
+                $transfusaoOcorrencia->data_transfusao = $request->data_complicacao;
+                $transfusaoOcorrencia->volume_transfusao = $request->volume_transfusao;
+                $transfusaoOcorrencia->paciente_id = $pacienteId;
+                $transfusaoOcorrencia->save();
+            }
         }
 
         if($request->has(['tipo_transfusao_id', 'data_transfusao', 'volume_transfusao'])) {
