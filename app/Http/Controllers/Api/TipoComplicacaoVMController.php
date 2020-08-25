50c5a78ff3e156e3b2573e82121260ac69ca6e94
moisesrodriguesdev <moisesabreurodrigues@gmail.com>
2020-08-25 08:37:33 -0300

    adiciona documentacao swagger #56

--- app/Http/Controllers/Api/TipoComplicacaoVMController.php
+++ app/Http/Controllers/Api/TipoComplicacaoVMController.php
@@ -8,6 +8,42 @@ use Illuminate\Http\Request;
 
 class TipoComplicacaoVMController extends Controller
 {
+    /**
+     * Listar tipos de complicações ventilação mecânica
+     *
+     * @OA\Get(
+     *      path="/api/tipos-complicacao-vm",
+     *      operationId="getTiposComplicacaoesVM",
+     *      tags={"Recursos"},
+     *      summary="Lista tipos de complicações relacionados a ventilação mecânica",
+     *      description="Retorna todos tipos de complicações relacionados a ventilação mecânica",
+     *      security={{"apiAuth":{}}},
+     *      @OA\Response(
+     *          response=200,
+     *          description="Executado com sucesso",
+     *          content={
+     *              @OA\MediaType(
+     *                  mediaType="application/json",
+     *                  @OA\Schema(
+     *                      example={
+     *                          {
+     *                              "id": 1,
+     *                              "descricao": "Pneumotórax"
+     *                          },
+     *                          {
+     *                              "id": 2,
+     *                              "descricao": "Extubação acidental"
+     *                          },
+     *                      }
+     *                  )
+     *              )
+     *          }
+     *       ),
+     *      @OA\Response(response=401, description="Unauthorized"),
+     * )
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index()
     {
         return TipoComplicacaoVM::all()->toArray();
