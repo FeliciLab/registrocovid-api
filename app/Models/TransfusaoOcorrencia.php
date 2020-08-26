9607efdd07abbf9554e15e87afb539cb0d3d582e
Fernando Fabricio dos Santos <fernandofabricio.d@thoughtworks.com>
2020-08-26 09:45:45 -0300

    Fix typo

--- app/Models/TransfusaoOcorrencia.php
+++ app/Models/TransfusaoOcorrencia.php
@@ -16,10 +16,10 @@ class TransfusaoOcorrencia extends Model
     protected $hidden = ['created_at', 'updated_at', 'tipo_transfusao_id', 'paciente_id'];
 
     protected $with = [
-        'tiposTransfussoes'
+        'tiposTransfusao'
     ];
 
-    public function tiposTransfussoes()
+    public function tiposTransfusao()
     {
         return $this->hasOne(TipoTransfusao::class, 'id', 'tipo_transfusao_id');
     }
