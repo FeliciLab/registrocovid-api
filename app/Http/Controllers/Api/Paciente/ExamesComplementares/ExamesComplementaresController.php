df594c3e6b67bf4f41e26ad2de3e4e547ea2ecc6
Edely Gomes <edelygomes@gmail.com>
2020-08-31 09:13:31 -0300

    #59 - Finaliza Testes e Doc/Swagger

--- app/Http/Controllers/Api/Paciente/ExamesComplementares/ExamesComplementaresController.php
+++ app/Http/Controllers/Api/Paciente/ExamesComplementares/ExamesComplementaresController.php
@@ -10,6 +10,48 @@ use App\Model\ExameComplementar;
 class ExamesComplementaresController extends Controller
 {
 
+    /**
+     * Cadastra exames complementares no sistema
+     * 
+     * @OA\Post(
+     *      path="/api/exames-complementares",
+     *      operationId="storeExamesComplementares",
+     *      tags={"Recursos"},
+     *      summary="Cadastra exames complementares",
+     *      description="Cadastra exames complementares no sistema",
+     *      security={{"apiAuth":{}}},
+     *      @OA\RequestBody(
+     *          description="",
+     *          required=true,
+     *          @OA\JsonContent(
+     *              @OA\Property(property="descricao", type="string")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Recurso criado com sucesso",
+     *          content={
+     *              @OA\MediaType(
+     *                  mediaType="application/json",
+     *                  @OA\Schema(
+     *                      example={
+     *                          "examescomplementares" : [
+     *                               {
+     *                                   "tipo_exames_complementares_id" : 1,
+     *                                   "data" : "2020-04-01",
+     *                                   "resultado": "Resultado do Exame"
+     *                               }
+     *                               
+     *                           ]
+     *                      }
+     *                  )
+     *              )
+     *          }
+     *       ),
+     *      @OA\Response(response=401, description="Unauthorized"),
+     * )
+     * @return JsonResponse
+     */
     public function store(ExamesComplementaresRequest $request, $pacienteId)
     {
         try{
@@ -18,14 +60,57 @@ class ExamesComplementaresController extends Controller
             );
             
         }catch (\Exception $e) {
-            $response = new ErrorMessage($e->getMessage());
+            $response = $e->getMessage();
             $statusCode = 500;
         }
         
         return response()->json($response, $statusCode);
     }
 
-
+    /**
+     * Lista os exames complementares de um paciente
+     * 
+     * @OA\Get(
+     *      path="/api/exames-complementares",
+     *      operationId="getExamesComplementares",
+     *      tags={"Recursos"},
+     *      summary="Lista os exames complementares de um paciente",
+     *      description="Retorna todos os exames complementares de um paciente cadastrados no sistema",
+     *      security={{"apiAuth":{}}},
+     *      @OA\Response(
+     *          response=200,
+     *          description="Executado com sucesso",
+     *          content={
+     *              @OA\MediaType(
+     *                  mediaType="application/json",
+     *                  @OA\Schema(
+     *                      example={
+     *                          [
+     *                               {
+     *                                   "id": 1,
+     *                                   "data": "2020-04-01",
+     *                                   "resultado": "Texto do Resultado",
+     *                                   "tipo_exame_id": 1,
+     *                                   "tipo_exame_descricao": "Tomografia computadorizada de tórax"
+     *                               },
+     *                               {
+     *                                   "id": 2,
+     *                                   "data": "2020-04-01",
+     *                                   "resultado": "Texto do Resultado",
+     *                                   "tipo_exame_id": 2,
+     *                                   "tipo_exame_descricao": "Eletrocardiograma"
+     *                               }
+     *                           ]
+     *                      }
+     *                  )
+     *              )
+     *          }
+     *       ),
+     *      @OA\Response(response=401, description="Unauthorized"),
+     * )
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index($pacienteId)
     {
         [$response, $statusCode] = ExamesComplementares::mostrarExamesComplementares($pacienteId);
--- app/Http/Controllers/Api/TipoExamesComplementaresController.php
+++ app/Http/Controllers/Api/TipoExamesComplementaresController.php
@@ -8,6 +8,44 @@ use App\Models\TipoExameComplementar;
 class TipoExamesComplementaresController extends Controller
 {
 
+    /**
+     * Lista os tipos de exames complementares cadastrados no sistema
+     * 
+     * @OA\Get(
+     *      path="/api/tipos-exames-complementares",
+     *      operationId="getTipoExamesComplementares",
+     *      tags={"Recursos"},
+     *      summary="Lista os tipos de exames complementares",
+     *      description="Retorna todos os tipos de exames complementares cadastrados no sistema",
+     *      security={{"apiAuth":{}}},
+     *      @OA\Response(
+     *          response=200,
+     *          description="Executado com sucesso",
+     *          content={
+     *              @OA\MediaType(
+     *                  mediaType="application/json",
+     *                  @OA\Schema(
+     *                      example={
+     *                         [
+     *                              {
+     *                                   "id": 1,
+     *                                   "descricao": "Tomografia computadorizada de tórax"
+     *                               },
+     *                               {
+     *                                   "id": 2,
+     *                                   "descricao": "Eletrocardiograma"
+     *                               }
+     *                           ]
+     *                      }
+     *                  )
+     *              )
+     *          }
+     *       ),
+     *      @OA\Response(response=401, description="Unauthorized"),
+     * )
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         return TipoExameComplementar::all();
