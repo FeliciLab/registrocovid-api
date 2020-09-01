e61027267f731ae831d0b851d48a216b8fb1ed17
moisesrodriguesdev <moisesabreurodrigues@gmail.com>
2020-09-01 00:14:48 -0300

    corrigi validacao e adiciona tipo_transfusao_id na criacao dos dados 89

--- app/Http/Requests/ComplicacaoVentilacaoMecanicaRequest.php
+++ app/Http/Requests/ComplicacaoVentilacaoMecanicaRequest.php
@@ -18,7 +18,6 @@ class ComplicacaoVentilacaoMecanicaRequest extends FormRequest
             'data_complicacao' => 'required_with:tipo_complicacao_id|date',
             'descricao' => 'nullable|string',
             'tipo_transfusao_id' => 'nullable|integer|exists:tipos_transfusao,id',
-            'data_transfusao' => 'required_with:tipo_transfusao_id|date',
             'volume_transfusao' => 'nullable|regex:/^\d+(\.\d{1,2})?$/'
         ];
     }
