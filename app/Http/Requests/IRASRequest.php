762df0df1bb586eb3522b815c62a4ae34b827bac
Edely Gomes <edelygomes@gmail.com>
2020-09-03 17:14:42 -0300

    WIP

--- app/Http/Requests/IRASRequest.php
+++ app/Http/Requests/IRASRequest.php
@@ -17,7 +17,7 @@ class IRASRequest extends FormRequest
         return [
             'iras.*.tipo_iras_id' => 'integer',
             'iras.*.data' => 'date',
-            'iras.*.descricao' => 'string',
+            'iras.*.descricao' => 'nullable|string',
         ];
     }
 }
