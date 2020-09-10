ac0193833c4ad05d711af3f87bdf4d73b0996db1
moisesrodriguesdev <moisesabreurodrigues@gmail.com>
2020-09-09 22:35:58 -0300

    Documenta recursos #73

--- app/Http/Controllers/Api/Paciente/Desfecho/DesfechoController.php
+++ app/Http/Controllers/Api/Paciente/Desfecho/DesfechoController.php
@@ -11,6 +11,73 @@ use App\Http\Resources\Desfecho as DesfechoResource;
 
 class DesfechoController extends Controller
 {
+
+    /**
+     * Exibi desfechos do paciente
+     *
+     * @OA\Get(
+     *      path="/api/pacientes/{pacienteId}/desfecho",
+     *      operationId="getDesfecho",
+     *      tags={"Paciente"},
+     *      summary="Exibi desfechos do paciente cadastrada",
+     *      description="Mostra desfechos",
+     *      security={{"apiAuth":{}}},
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="ID do paciente previamente cadastrado",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Executado com sucesso",
+     *          content={
+     *              @OA\MediaType(
+     *                  mediaType="application/json",
+     *                  @OA\Schema(
+     *                      example={
+     *                          "data": {
+     *                              {
+     *                                   "id": 1,
+     *                                   "tipo_desfecho": "Alta hospitalar",
+     *                                   "tipo_autocuidado": {
+     *                                      "id": 1,
+     *                                      "descricao": "Mesma antes da doença"
+     *                                   },
+     *                                   "instituicao_transferencia": null,
+     *                                   "data": "2020-09-08",
+     *                                   "causa_obito": null,
+     *                                   "obito_menos_24h": null,
+     *                                   "obito_em_vm": null,
+     *                                   "obito_em_uti": null,
+     *                                   "tipo_cuidado_paliativo": null
+     *                              },
+     *                              {
+     *                                   "id": 2,
+     *                                   "tipo_desfecho": "Óbito",
+     *                                   "tipo_autocuidado": null,
+     *                                   "instituicao_transferencia": null,
+     *                                   "data": "2020-09-11",
+     *                                   "causa_obito": "Causa do óbito desconhecida",
+     *                                   "obito_menos_24h": false,
+     *                                   "obito_em_vm": true,
+     *                                   "obito_em_uti": false,
+     *                                   "tipo_cuidado_paliativo": null
+     *                              }
+     *                          }
+     *                      }
+     *                  )
+     *              )
+     *          }
+     *       ),
+     *      @OA\Response(response=401, description="Unauthorized"),
+     * )
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index($pacienteId)
     {
         $desfecho = Desfecho::where('paciente_id', $pacienteId)->get();
@@ -26,6 +93,68 @@ class DesfechoController extends Controller
         return DesfechoResource::collection($desfecho);
     }
 
+
+    /**
+     * Cadastra desfecho do paciente no sistema
+     *
+     * @OA\Post(
+     *      path="/api/pacientes/{pacienteId}/desfecho",
+     *      operationId="storeDesfecho",
+     *      tags={"Paciente"},
+     *      summary="Cadastro de desfecho",
+     *      description="Cadastro desfecho",
+     *      security={{"apiAuth":{}}},
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="ID do paciente previamente cadastrado",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *              required={"tipo_desfecho_id","data"},
+     *              @OA\Property(property="tipo_desfecho_id", type="integer", example=1),
+     *              @OA\Property(property="tipo_autocuidado_id", type="integer", example=2),
+     *              @OA\Property(property="data", type="date", example="2020-09-10"),
+     *              @OA\Property(property="instituicao_transferencia_id", type="integer", example=1),
+     *              @OA\Property(property="tipo_cuidado_paliativo_id", type="integer", example=1),
+     *              @OA\Property(property="obito_menos_24h", type="boolean", example="true"),
+     *              @OA\Property(property="obito_em_vm", type="boolean", example="true"),
+     *              @OA\Property(property="obito_em_uti", type="boolean", example="false"),
+     *              @OA\Property(property="causa_obito", type="string", example="Causa óbito do paciente"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Executado com sucesso",
+     *          content={
+     *              @OA\MediaType(
+     *                  mediaType="application/json",
+     *                  @OA\Schema(
+     *                      example={
+     *                          "message": "Desfecho cadastrado com sucesso",
+     *                          "desfecho": {
+     *                              {
+     *                                  "tipo_desfecho_id": 1,
+     *                                  "tipo_autocuidado_id": 1,
+     *                                  "data": "2020-09-08",
+     *                                  "id": 1
+     *                              },
+     *                           }
+     *                      }
+     *                  )
+     *              )
+     *          }
+     *       ),
+     *      @OA\Response(response=401, description="Unauthorized")
+     * )
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function store(DesfechoStoreRequest $request, $pacienteId)
     {
         $desfechoCollection = new Collection();
--- app/Http/Controllers/Api/TipoCuidadoPaliativoController.php
+++ app/Http/Controllers/Api/TipoCuidadoPaliativoController.php
@@ -8,6 +8,51 @@ use Illuminate\Http\Request;
 
 class TipoCuidadoPaliativoController extends Controller
 {
+
+    /**
+     * Lista tipos de cuidados paliativos
+     *
+     * @OA\Get(
+     *      path="/api/tipos-cuidado-paliativo",
+     *      operationId="getTipoCuidadoPaliativo",
+     *      tags={"Recursos"},
+     *      summary="Lista tipos de cuidados paliativos",
+     *      description="Retorna todos tipos de cuidados paliativos cadastrados no sistema",
+     *      security={{"apiAuth":{}}},
+     *      @OA\Response(
+     *          response=200,
+     *          description="Executado com sucesso",
+     *          content={
+     *              @OA\MediaType(
+     *                  mediaType="application/json",
+     *                  @OA\Schema(
+     *                      example={
+     *                          {
+     *                              "id": 1,
+     *                              "descricao": "Sim"
+     *                          },
+     *                          {
+     *                               "id": 2,
+     *                               "descricao": "Não"
+     *                          },
+     *                          {
+     *                               "id": 3,
+     *                               "descricao": "Aguardando aval equipe CP"
+     *                          },
+     *                          {
+     *                               "id": 4,
+     *                               "descricao": "Não RCP por gravidade do caso"
+     *                          }
+     *                      }
+     *                  )
+     *              )
+     *          }
+     *       ),
+     *      @OA\Response(response=401, description="Unauthorized"),
+     * )
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         return TipoCuidadoPaliativo::all()->toArray();
