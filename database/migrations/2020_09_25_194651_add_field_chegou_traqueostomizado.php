586fac21876368a70a71353dc68f71ecac370cad
Felipe Vidal <felipedesousavidal@gmail.com>
2020-09-28 15:07:47 -0300

    Update 2020_09_25_194651_add_field_chegou_traqueostomizado.php

--- database/migrations/2020_09_25_194651_add_field_chegou_traqueostomizado.php
+++ database/migrations/2020_09_25_194651_add_field_chegou_traqueostomizado.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdatePacientes extends Migration
+class AddFieldChegouTraqueostomizado extends Migration
 {
     /**
      * Run the migrations.
