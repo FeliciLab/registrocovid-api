a04fd073df2d03f8864dd1a523c6b54dd0443b3a
Felipe Vidal <felipedesousavidal@gmail.com>
2020-10-29 18:02:33 -0300

    fix tests

--- tests/Feature/Paciente/EvolucoesDiarias/MostrarEvolucoesDiariasPacienteTest.php
+++ tests/Feature/Paciente/EvolucoesDiarias/MostrarEvolucoesDiariasPacienteTest.php
@@ -57,7 +57,7 @@ class MostrarEvolucoesDiariasPacienteTest extends TestCase
             "pressao_sistolica",
             "pressao_diastolica",
             "frequencia_cardiaca",
-            "ascultura_pulmonar",
+            "ausculta_pulmonar",
             "oximetria",
             "escala_glasgow",
             "created_at",
