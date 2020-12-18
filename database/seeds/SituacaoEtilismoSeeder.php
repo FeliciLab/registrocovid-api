d9f3c71c766da2724dea4b208221eda427d9626d
Felipe Vidal <felipedesousavidal@gmail.com>
2020-12-18 14:57:18 -0300

    fix deskcheck

--- database/seeds/SituacaoEtilismoSeeder.php
+++ database/seeds/SituacaoEtilismoSeeder.php
@@ -13,7 +13,7 @@ class SituacaoEtilismoSeeder extends Seeder
     public function run()
     {
         DB::table('situacao_etilismo')->insert(['id' => 1, 'descricao' => 'Etilista']);
-        DB::table('situacao_etilismo')->insert(['id' => 2, 'descricao' => 'Ex- etilista']);
+        DB::table('situacao_etilismo')->insert(['id' => 2, 'descricao' => 'Ex-etilista']);
         DB::table('situacao_etilismo')->insert(['id' => 3, 'descricao' => 'Não etilista']);
         DB::table('situacao_etilismo')->insert(['id' => 4, 'descricao' => 'Não Informado']);
     }
