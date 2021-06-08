7147141bc535b204163f05f0d79c1438f190a9ad
Victor Magalhães Pacheco <victor.magalhaesp@gmail.com>
2021-06-08 14:26:02 -0300

    Feature/prontuario instituicao validacao unique (#212)
    * Atualizando bug da terapia transfusional
    
    * Adicionando validação prontuario e instituição unique
    
    * Adicionando migration para remover unique do prontuario no banco

--- app/Http/Requests/TerapiaTransfusionalRequest.php
+++ app/Http/Requests/TerapiaTransfusionalRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class TerapiaTranfusionalRequest extends FormRequest
+class TerapiaTransfusionalRequest extends FormRequest
 {
 
     /**
